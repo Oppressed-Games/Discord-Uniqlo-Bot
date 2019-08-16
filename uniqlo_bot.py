e844d4461b036cd28f0a1ad76e96c7a1f91f483d
submaureen <maureenmgloria@gmail.com>
2019-08-16 18:27:38 -0500

    checking for in stock badge

--- uniqlo_bot.py
+++ uniqlo_bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 import config
+import re
 from discord.ext import commands
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -16,12 +17,12 @@ async def on_ready():
     print("am here")
 
 
-@bot.command(pass_context=True)
-async def pastebin(ctx):
-    posts = get_paste()
-    for post in posts:
-        if post != " ":
-            await ctx.send(post.text)
+#@bot.command(pass_context=True)
+#async def foo(ctx, url):
+#    if re.match('^https?://(www.)?uniqlo.com/.*$', url) is not None:
+#        print("wow")
+#    else:
+#        print("not wow")
 
 
 @bot.command(pass_context=True)
@@ -30,20 +31,43 @@ async def skormt(ctx):
     await ctx.send(skirt)
 
 
-def get_paste():
-    # testing browser
-    driver.get("http://pastebin.com/LhX55TNJ")
-    posts = driver.find_elements_by_class_name("de1")
-    return posts
+@bot.command(pass_context=True)
+async def test(ctx, cmd):
+    print(cmd)
+    if cmd == "@everyone":
+        await ctx.send("no")
+    await ctx.send(cmd)
+
+
+@bot.command(pass_context=True)
+async def check(ctx, url):
+    await ctx.send(get_stock(url))
 
 
 def get_image():
     # send to uniqlo
     driver.get("https://www.uniqlo.com/us/en/women-front-button-circular-skirt-417764.html")
-
     image = driver.find_elements_by_class_name("primary-image")
     imageSRC = image[0].get_attribute("src")
     return imageSRC
+
+
+def get_stock(url):
+    result = ""
+    driver.get(url)
+    if re.match('^https?://(www.)?uniqlo.com/.*$', url) is not None:
+        if len(driver.find_elements_by_class_name("outofstockbadge")) > 0:
+            result = "Out of stock"
+            return result
+        else:
+            result = "In stock"
+            return result
+    else:
+        result = "that's not uniqlo"
+        return result
+
+
+
 
 #bot token
 bot.run(config.token, reconnect=True)
