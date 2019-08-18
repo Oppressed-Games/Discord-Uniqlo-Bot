fe1cc19ecaa1ac639b459029b0206ba0219445a0
DJ <dj9123@live.com>
2019-08-17 23:07:14 -0500

    Output messages to client on success or error

--- uniqlo_bot.py
+++ uniqlo_bot.py
@@ -7,24 +7,58 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="$")
 
 
+async def setup_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('You do not have permission to use this command.')
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please enter the name of the cog as an argument.')
+    else:
+        await ctx.send(error)
+
+
+async def setup_complete(ctx, setup_action, extension):
+    await ctx.send(f'Successfully {setup_action}ed {extension} cog.')
+
+
 @bot.event
 async def on_ready():
     print("am here")
 
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
+    await setup_complete(ctx, 'load', extension)
+
+
+@load.error
+async def load_error(ctx, error):
+    await setup_error(ctx, error)
 
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
+    await setup_complete(ctx, 'unload', extension)
+
+
+@unload.error
+async def unload_error(ctx, error):
+    await setup_error(ctx, error)
 
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def reload(ctx, extension):
     bot.reload_extension(f'cogs.{extension}')
+    await setup_complete(ctx, 'reload', extension)
+
+
+@reload.error
+async def reload_error(ctx, error):
+    await setup_error(ctx, error)
 
 
 for filename in os.listdir('./cogs'):
