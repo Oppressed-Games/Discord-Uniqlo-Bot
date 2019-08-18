6ec33c5e74dd88408551e694ff7d472a96695983
DJ <dj9123@live.com>
2019-08-17 23:50:37 -0500

    Move driver methods into class and add cog_unload

--- cogs/test.py
+++ cogs/test.py
@@ -8,54 +8,20 @@ from discord.ext import commands
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# driver stuff ; find chromedriver.exe path
-# chrome_path = r"C:..."
-driver = webdriver.Chrome()
-
-
-async def get_image(ctx):
-    # send to uniqlo
-    start = timer()
-    driver.get("https://www.uniqlo.com/us/en/women-front-button-circular-skirt-417764.html")
-    image = driver.find_elements_by_class_name("primary-image")
-    image_src = image[0].get_attribute("src")
-    end = timer()
-    await ctx.send(end - start)
-    return image_src
-
-
-async def get_image_bs4(ctx):
-    # send to uniqlo
-    start = timer()
-    res = requests.get("https://www.uniqlo.com/us/en/women-front-button-circular-skirt-417764.html")
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, "lxml")
-    image = soup.select('img[class="primary-image"]')
-    image_src = image[0].get('src')
-    end = timer()
-    await ctx.send(end - start)
-    return image_src
-
-
-def get_stock(url):
-    result = ""
-    driver.get(url)
-    if re.match('^https?://(www.)?uniqlo.com/.*$', url) is not None:
-        if len(driver.find_elements_by_class_name("outofstockbadge")) > 0:
-            result = "Out of stock"
-            return result
-        else:
-            result = "In stock"
-            return result
-    else:
-        result = "that's not uniqlo"
-        return result
-
 
 class Test(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        # driver stuff ; find chromedriver.exe path
+        # chrome_path = r"C:..."
+        self.driver = webdriver.Chrome()
+        print('Opened web driver.')
+
+    def cog_unload(self):
+        if hasattr(self, 'driver'):
+            self.driver.quit()
+            print('Closed web driver.')
 
     # Events
 
@@ -73,12 +39,12 @@ class Test(commands.Cog):
 
     @commands.command()
     async def skormt(self, ctx):
-        skirt = await get_image(ctx)
+        skirt = await self.get_image(self, ctx)
         await ctx.send(skirt)
 
     @commands.command()
     async def skormt_bs4(self, ctx):
-        skirt = await get_image_bs4(ctx)
+        skirt = await self.get_image_bs4(self, ctx)
         await ctx.send(skirt)
 
     @commands.command()
@@ -90,7 +56,7 @@ class Test(commands.Cog):
 
     @commands.command()
     async def check(self, ctx, url):
-        await ctx.send(get_stock(url))
+        await ctx.send(self.get_stock(self, url))
 
     @commands.command()
     async def foo(self, ctx, url):
@@ -98,6 +64,47 @@ class Test(commands.Cog):
             ctx.send("wow")
         else:
             ctx.send("not wow")
+
+    # Helpers
+
+    @staticmethod
+    async def get_image(self, ctx):
+        # send to uniqlo
+        start = timer()
+        self.driver.get("https://www.uniqlo.com/us/en/women-front-button-circular-skirt-417764.html")
+        image = self.driver.find_elements_by_class_name("primary-image")
+        image_src = image[0].get_attribute("src")
+        end = timer()
+        await ctx.send(end - start)
+        return image_src
+
+    @staticmethod
+    async def get_image_bs4(ctx):
+        # send to uniqlo
+        start = timer()
+        res = requests.get("https://www.uniqlo.com/us/en/women-front-button-circular-skirt-417764.html")
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, "lxml")
+        image = soup.select('img[class="primary-image"]')
+        image_src = image[0].get('src')
+        end = timer()
+        await ctx.send(end - start)
+        return image_src
+
+    @staticmethod
+    def get_stock(self, url):
+        result = ""
+        self.driver.get(url)
+        if re.match('^https?://(www.)?uniqlo.com/.*$', url) is not None:
+            if len(self.driver.find_elements_by_class_name("outofstockbadge")) > 0:
+                result = "Out of stock"
+                return result
+            else:
+                result = "In stock"
+                return result
+        else:
+            result = "that's not uniqlo"
+            return result
 
 
 def setup(client):
